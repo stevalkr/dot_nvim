442519acf2efed9afd6b71e00537d2746d68cddd
Walker <zero.stevewalker@gmail.com>
2023-06-04 10:10:25 +0800

    transparent background

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,6 +4,9 @@ return {
     'shaunsingh/nord.nvim',
     config = function()
       vim.cmd [[colorscheme nord]]
+      vim.cmd [[highlight Normal guibg=NONE ctermbg=NONE]]
+      vim.cmd [[highlight NonText guibg=NONE ctermbg=NONE]]
+      vim.cmd [[highlight SignColumn guibg=NONE ctermbg=NONE]]
     end
   }
 
