44a0f1996adac167958467d151bb3705017b0870
Steve Walker <zero.stevewalker@gmail.com>
2024-05-09 16:14:41 +0800

    change copilot keymap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -43,8 +43,15 @@ return {
 
   {
     'github/copilot.vim',
-    event = { 'InsertEnter' },
-    build = '<Cmd>Copilot setup'
+    event = { 'BufReadPost' },
+    build = '<Cmd>Copilot setup',
+    config = function(_, _opt)
+      vim.keymap.set('i', '<C-CR>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false
+      })
+      vim.g.copilot_no_tab_map = true
+    end
   }
 
 }
